e5c914ffa13fe787553baaa73374feff03e0a88f
cheyang <cheyang@163.com>
2018-09-04 11:07:00 +0800

    init data support (#48)

--- cmd/arena/commands/data_info.go
+++ cmd/arena/commands/data_info.go
@@ -13,34 +13,3 @@
 // limitations under the License.
 
 package commands
-
-import (
-	"github.com/spf13/cobra"
-	// podv1 "k8s.io/api/core/v1"
-)
-
-var (
-	topLong = `Display Resource (GPU) usage.
-
-Available Commands:
-  node        Display Resource (GPU) usage of nodes
-  job         Display Resource (GPU) usage of pods
-    `
-)
-
-func NewTopCommand() *cobra.Command {
-	var command = &cobra.Command{
-		Use:   "top",
-		Short: "Display Resource (GPU) usage.",
-		Long:  topLong,
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.HelpFunc()(cmd, args)
-		},
-	}
-
-	// create subcommands
-	command.AddCommand(NewTopNodeCommand())
-	command.AddCommand(NewTopJobCommand())
-
-	return command
-}
