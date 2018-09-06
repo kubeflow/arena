348ee64da17d8a9532b385247f6c6dda231bef0c
cheyang <cheyang@163.com>
2018-09-06 14:15:46 +0800

    add document for data volume (#50)
    fix wording

--- cmd/arena/commands/data_list.go
+++ cmd/arena/commands/data_list.go
@@ -71,6 +71,7 @@ func NewDataListCommand() *cobra.Command {
 	return command
 }
 
+// Display the data volume
 func displayDataVolume(pvcList *v1.PersistentVolumeClaimList) {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 	if allNamespaces {
@@ -84,6 +85,13 @@ func displayDataVolume(pvcList *v1.PersistentVolumeClaimList) {
 	}
 
 	for _, item := range pvcList.Items {
+		if item.ObjectMeta.DeletionTimestamp != nil || item.Status.Phase != v1.ClaimBound {
+			log.Debugf("skip the data is not ready: %s", item.Name)
+			continue
+		}
+
+		// storage := item.Status.Capacity[v1.ResourceStorage]
+		// capacity := storage.String()
 		description := item.Annotations[dataDescritpion]
 		owner := item.Annotations[dataOwner]
 
