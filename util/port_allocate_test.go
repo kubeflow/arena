0dade58dbeb54eeda25b9a22835cc8796884b46a
xiaozhouX <xuxiaozhou93@gmail.com>
2018-09-30 14:03:11 +0800

    fix test code

--- util/port_allocate_test.go
+++ util/port_allocate_test.go
@@ -9,14 +9,18 @@ import (
 )
 
 func getClientSet(t *testing.T) *kubernetes.Clientset {
-	// The "usual" clientcmd/kubectl flags
 	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+
 	loadingRules.DefaultClientConfig = &clientcmd.DefaultClientConfig
 	overrides := clientcmd.ConfigOverrides{}
 	clientConfig := clientcmd.NewInteractiveDeferredLoadingClientConfig(loadingRules, &overrides, os.Stdin)
 	restConfig, err := clientConfig.ClientConfig()
 	if err != nil {
-		t.Errorf("failed to initclient, %++v", err)
+		t.Logf("failed to initclient, %++v", err)
+	}
+	if restConfig == nil {
+		t.Logf("Kube Client is not setup")
+		return nil
 	}
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(restConfig)
@@ -26,8 +30,11 @@ func getClientSet(t *testing.T) *kubernetes.Clientset {
 	return clientset
 }
 
-func TestGetAvailablePort(t *testing.T)  {
+func TestSelectAvailablePort(t *testing.T)  {
 	clientset := getClientSet(t)
+	if clientset == nil {
+		t.Skip("kubeclient not setup")
+	}
 	port1, err := SelectAvailablePort(clientset)
 	if err != nil {
 		t.Errorf("failed to SelectAvailablePort, %++v", err)
@@ -40,7 +47,7 @@ func TestGetAvailablePort(t *testing.T)  {
 	}
 	t.Logf("port is %d", port2)
 	if port2 != port1 + 1 {
-		t.Errorf("Port should be %s, when latest port is %d", port1 + 1, port1)
+		t.Errorf("Port should be %d, when latest port is %d", port1 + 1, port1)
 	}
 
 	k8sClusterUsedPorts = []int {30000, 30001}
