654d38e721528b398907e1b6c5efa16f95020418
cheyang <cheyang@163.com>
2018-11-06 17:05:17 +0800

    support load balancer

--- cmd/arena/commands/dashboard_helper.go
+++ cmd/arena/commands/dashboard_helper.go
@@ -15,7 +15,7 @@
 package commands
 
 import (
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
