c9de5f0b8156478b105a8a35ef573f9146478410
xiaozhouX <xuxiaozhou93@gmail.com>
2018-11-16 16:28:28 +0800

    add test skip when clientSet is unset

--- cmd/arena/commands/gpu_info_test.go
+++ cmd/arena/commands/gpu_info_test.go
@@ -7,7 +7,10 @@ import (
 
 func TestGetGpuInfo(t *testing.T) {
 	clientset := util.GetClientSetForTest(t)
-	gpuMetrics, err := GetGpuInfo(clientset, []string{"tensorrt-resnet50-744868bd7d-dl799"})
+	if clientset == nil {
+		t.Skip("kubeclient not setup")
+	}
+	gpuMetrics, err := GetGpuInfo(clientset, "prometheus-svc", `avg (nvidia_gpu_duty_cycle{pod_name=~"tensorrt-resnet50-744868bd7d-dl799", container_name!=""}) by (pod_name)`)
 	if err != nil {
 		t.Errorf("failed to get gpuInfo, err: %++v", err)
 	}
