67561f7a78bfd294318c45ad49dc79d3abcb785d
cheyang <cheyang@163.com>
2019-01-22 16:58:13 +0800

    add training type to configmap

--- util/helm/helm_advanced.go
+++ util/helm/helm_advanced.go
@@ -36,8 +36,8 @@ func GenerateValueFile(values interface{}) (valueFileName string, err error) {
 * returns generated template file: templateFileName
  */
 func GenerateHelmTemplate(name string, namespace string, valueFileName string, chartName string) (templateFileName string, err error) {
-	tempName := fmt.Sprintf("/tmp/%s.yaml", name)
-	templateFile, err := ioutil.TempFile(os.TempDir(), tempName)
+	tempName := fmt.Sprintf("%s.*.yaml", name)
+	templateFile, err := ioutil.TempFile("", tempName)
 	if err != nil {
 		return templateFileName, err
 	}
