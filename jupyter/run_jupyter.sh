628cba709c594d35c7f9f66948585b4b72dc7fa5
cheyang <cheyang@163.com>
2019-02-25 20:32:37 +0800

    add download repo

--- jupyter/run_jupyter.sh
+++ jupyter/run_jupyter.sh
@@ -6,4 +6,11 @@ mkdir -p $CONF_DIR
 
 cp /jupyter_notebook_config.py $CONF_DIR
 
+set +e
+GIT_REPO=ai-starter
+if [ ! -d "$GIT_REPO" ]; then
+  git clone https://github.com/AliyunContainerService/$GIT_REPO.git
+fi
+set -e
+
 jupyter notebook --allow-root "$@"
