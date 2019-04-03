9d164db9e4d5525426e0c816b8625c6c66ddf506
xiaozhouX <xuxiaozhou93@gmail.com>
2019-04-03 18:18:29 +0800

    fix tensorboard internalIP; remove NotReady Node in top node

--- cmd/arena/commands/tensorboard.go
+++ cmd/arena/commands/tensorboard.go
@@ -121,23 +121,17 @@ func tensorboardURL(name, namespace string) (url string, err error) {
 	findReadyNode := false
 
 	for _, item := range nodeList.Items {
-		for _, condition := range item.Status.Conditions {
-			if condition.Type == "Ready" {
-				if condition.Status == "True" {
-					node = item
-					findReadyNode = true
-					break
-				}
-			}
+		if isNodeReady(item) {
+			node = item
+			findReadyNode = true
+			break
 		}
 	}
 
 	if !findReadyNode {
 		return "", fmt.Errorf("Failed to find the ready node for exporting tensorboard.")
 	}
-
-	address = node.Status.Addresses[0].Address
-	url = fmt.Sprintf("http://%s:%d", address, port)
+	url = fmt.Sprintf("http://%s:%d", getNodeInternalAddress(node), port)
 
 	return url, nil
 }
