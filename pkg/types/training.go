e64b8b71efe034a82531f66850b7894b638b1a88
cheyang <cheyang@163.com>
2019-04-11 22:02:31 +0800

    add the training job status

---
+++ pkg/types/training.go
@@ -0,0 +1,45 @@
+package types
+
+import "k8s.io/api/core/v1"
+
+type TrainingJobInfo struct {
+	// The name of the training job
+	Name string `json:"name"`
+	// The namespace of the training job
+	Namespace string `json:"namespace"`
+	// The time of the training job
+	Duration string `json:"duration"`
+	// The status of the Job
+	Status JobStatus `json:"status"`
+
+	// The training type of the job
+	Trainer string `json:"trainer"`
+
+	// The instances under the training job
+	Instances []Instance `json:"instances"`
+}
+
+// all the kinds of JobStatus
+type JobStatus string
+
+const (
+	// JobPending means the job is pending
+	JobPending JobStatus = "PENDING"
+	// JobRunning means the job is running
+	JobRunning JobStatus = "RUNNING"
+	// JobSucceeded means the job is Succeeded
+	JobSucceeded JobStatus = "SUCCEEDED"
+	// JobFailed means the job is failed
+	JobFailed JobStatus = "FAILED"
+)
+
+type Instance struct {
+	// the status of of instance
+	Status v1.PodPhase `json:"status"`
+	// the name of instance
+	Name string `json:"name"`
+	// the age of instance
+	Age string `json:"age"`
+	// the node instance runs on
+	Node string `json:"node"`
+}
