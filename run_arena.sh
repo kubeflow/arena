54570c51c701612c814c1a768c410f4eb06fe931
cheyang <cheyang@163.com>
2019-07-16 10:57:41 +0800

    update install script

--- run_arena.sh
+++ run_arena.sh
@@ -74,9 +74,13 @@ fi
 
 
 if [ -d "/host" ]; then
+    now=$(date "+%Y%m%d%H%M%S")
+    if [ -f "/host/usr/local/bin/arena" ]; then
+        mv /host/usr/local/bin/arena /host/usr/local/bin/arena-$now
+    fi
     cp /usr/local/bin/arena /host/usr/local/bin/arena
     if [ -d "/host/charts" ]; then
-        mv /host/charts /host/charts_$(date "+%Y%m%d%H%M%S")
+        mv /host/charts /host/charts-$now
     fi
     cp -r /charts /host
 fi
