b6cc0474beb21f2d969375e45c514f020c43a587
cheyang <cheyang@163.com>
2019-08-15 20:01:10 +0800

    fix make file

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x -e
+set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$(readlink "$0" || echo "$0")")"; pwd)"
 
