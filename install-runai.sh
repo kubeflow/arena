0929e93df6091f39dedd9d4d68da9e3cfefa2dde
Or Cherfas <or@run.ai>
2019-12-10 15:33:02 +0200

    fix for install script

--- install-runai.sh
+++ install-runai.sh
@@ -7,11 +7,11 @@ SCRIPT_CHARTS=${SCRIPT_FILES}/charts
 
 SCRIPT_DIR="$(cd "$(dirname "$(readlink "$0" || echo "$0")")"; pwd)"
 
-cp -rf $SCRIPT_DIR/bin/arena /usr/local/bin/${SCRIPT_NAME}
+cp -rf "$SCRIPT_DIR/bin/arena" /usr/local/bin/${SCRIPT_NAME}
 
 if [ -d "${SCRIPT_FILES}" ]; then
 rm -rf ${SCRIPT_FILES}
 fi
 
 mkdir ${SCRIPT_FILES}
-cp -R ${SCRIPT_DIR}/charts ${SCRIPT_CHARTS}
+cp -R "${SCRIPT_DIR}/charts" ${SCRIPT_CHARTS}
