31874b74c2c8e6b8865d71ccf9a23abf2d028ef6
happy2048 <2270020588@qq.com>
2020-05-08 14:23:19 +0800

    rename kubectl to arena-kubectl

--- install.sh
+++ install.sh
@@ -27,11 +27,10 @@ if [ `id -u` -ne 0 ]; then
     sudo_prefix="sudo"
 fi
 
-if ! which kubectl >/dev/null 2>&1; then
-	${sudo_prefix} cp $SCRIPT_DIR/bin/kubectl /usr/local/bin/kubectl
-fi
+rm -rf /usr/local/bin/arena-kubectl
+${sudo_prefix} cp $SCRIPT_DIR/bin/kubectl /usr/local/bin/arena-kubectl
 
-if ! kubectl cluster-info >/dev/null 2>&1; then
+if ! arena-kubectl cluster-info >/dev/null 2>&1; then
     log "Please setup kubeconfig correctly before installing arena"
     exit 1
 fi
@@ -60,32 +59,32 @@ if [ "$USE_LOADBALANCER" == "true" ]; then
 fi
 
 
-if ! kubectl get serviceaccount --all-namespaces | grep jobmon; then
-    kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/jobmon/jobmon-role.yaml
+if ! arena-kubectl get serviceaccount --all-namespaces | grep jobmon; then
+    arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/jobmon/jobmon-role.yaml
 fi
 
-if ! kubectl get serviceaccount --all-namespaces | grep tf-job-operator; then
-    kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-crd.yaml
-    kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-operator.yaml
+if ! arena-kubectl get serviceaccount --all-namespaces | grep tf-job-operator; then
+    arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-crd.yaml
+    arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-operator.yaml
 else
-    if kubectl get crd tfjobs.kubeflow.org -oyaml |grep -i 'version: v1alpha2'; then
-        kubectl delete -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-operator-v1alpha2.yaml
-        kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-crd.yaml
-        kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-operator.yaml
+    if arena-kubectl get crd tfjobs.kubeflow.org -oyaml |grep -i 'version: v1alpha2'; then
+        arena-kubectl delete -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-operator-v1alpha2.yaml
+        arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-crd.yaml
+        arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/tf-operator/tf-operator.yaml
     fi
 fi
-if ! kubectl get serviceaccount --all-namespaces | grep mpi-operator; then
-    kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/mpi-operator/mpi-operator.yaml
+if ! arena-kubectl get serviceaccount --all-namespaces | grep mpi-operator; then
+    arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/mpi-operator/mpi-operator.yaml
 fi
 
 if [ "$USE_PROMETHEUS" == "true" ]; then
     if [ "$PLATFORM" == "ack" ]; then
         sed -i 's|accelerator/nvidia_gpu|aliyun.accelerator/nvidia_count|g' $SCRIPT_DIR/kubernetes-artifacts/prometheus/gpu-exporter.yaml
     fi
-    if ! kubectl get serviceaccount --all-namespaces | grep prometheus; then
-     kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/prometheus/gpu-exporter.yaml
-     kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/prometheus/prometheus.yaml
-     kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/prometheus/grafana.yaml
+    if ! arena-kubectl get serviceaccount --all-namespaces | grep prometheus; then
+     arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/prometheus/gpu-exporter.yaml
+     arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/prometheus/prometheus.yaml
+     arena-kubectl apply -f $SCRIPT_DIR/kubernetes-artifacts/prometheus/grafana.yaml
     fi
 fi
 # set -e
