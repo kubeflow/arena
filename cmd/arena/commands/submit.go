c4035c486c1e4c003083f468544b71364ea94658
King <jiaqianjing@gmail.com>
2020-06-19 06:49:10 +0800

    support submit pytorch job by git #28289726 (#324)
    add pytorch-operator api & client about submiting pytorch job #28289726
    
    submit pytorch job #28289726
    
    workerCount - 1, because the master is also considered as a worker #28289726
    
    update charts about submiting pytorch job #28289726
    
    add charts, pytorch operator api&client, and support submit pytorchjob
    by git  #28289726

--- cmd/arena/commands/submit.go
+++ cmd/arena/commands/submit.go
@@ -119,6 +119,7 @@ func (s submitArgs) check() error {
 }
 
 // transform common parts of submitArgs
+// e.g. --data-dir、--data、--annotation、PodSecurityContext
 func (s *submitArgs) transform() (err error) {
 	// 1. handle data dirs
 	log.Debugf("dataDir: %v", dataDirs)
@@ -354,12 +355,15 @@ func NewSubmitCommand() *cobra.Command {
 
 	command.AddCommand(NewSubmitTFJobCommand())
 	command.AddCommand(NewSubmitMPIJobCommand())
+	// support pytorchjob
+	command.AddCommand(NewSubmitPyTorchJobCommand())
 	command.AddCommand(NewSubmitHorovodJobCommand())
 	// This will be deprecated soon.
 	command.AddCommand(NewSubmitStandaloneJobCommand())
 	command.AddCommand(NewSparkApplicationCommand())
 
 	command.AddCommand(NewVolcanoJobCommand())
+
 
 	return command
 }
