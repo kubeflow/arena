650d2ef0f8772a18a4fb089719074ef0376fd314
yu lin <735122171@qq.com>
2023-08-29 16:04:34 +0800

    Support maxSurge, livenessProbe, readinessProbe. (#983)
    Signed-off-by: Syulin7 <735122171@qq.com>

--- pkg/argsbuilder/serving_custom.go
+++ pkg/argsbuilder/serving_custom.go
@@ -19,7 +19,14 @@ import (
 	"strings"
 
 	"github.com/kubeflow/arena/pkg/apis/types"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+)
+
+const (
+	LivenessProbeActionOption  = "livenessProbeActionOption"
+	ReadinessProbeActionOption = "readinessProbeActionOption"
+	StartupProbeActionOption   = "StartupProbeActionOption"
 )
 
 type CustomServingArgsBuilder struct {
@@ -65,9 +72,31 @@ func (s *CustomServingArgsBuilder) AddCommandFlags(command *cobra.Command) {
 	for name := range s.subBuilders {
 		s.subBuilders[name].AddCommandFlags(command)
 	}
+
+	var (
+		livenessProbeActionOption  []string
+		readinessProbeActionOption []string
+		startupProbeActionOption   []string
+	)
+
 	command.Flags().IntVar(&s.args.Port, "port", 0, "the port of gRPC listening port, default is 0 represents that don't create service listening on this port")
 	command.Flags().IntVar(&s.args.RestfulPort, "restful-port", 0, "the port of RESTful listening port, default is 0 represents that don't create service listening on this port")
 	command.Flags().IntVar(&s.args.MetricsPort, "metrics-port", 0, "the port of metrics, default is 0 represents that don't create service listening on this port")
+	command.Flags().StringVar(&s.args.MaxSurge, "max-surge", "", "the maximum number of pods that can be created over the desired number of pods")
+	command.Flags().StringVar(&s.args.MaxUnavailable, "max-unavailable", "", "the maximum number of Pods that can be unavailable during the update process")
+	command.Flags().StringVar(&s.args.LivenessProbeAction, "liveness-probe-action", "", "the liveness probe action, support httpGet,exec,grpc,tcpSocket")
+	command.Flags().StringArrayVar(&livenessProbeActionOption, "liveness-probe-action-option", []string{}, `the liveness probe action option, usage: --liveness-probe-action-option="path: /healthz" or --liveness-probe-action-option="command=cat /tmp/healthy"`)
+	command.Flags().StringArrayVar(&s.args.LivenessProbeOption, "liveness-probe-option", []string{}, `the liveness probe option, usage: --liveness-probe-option="initialDelaySeconds: 3" or --liveness-probe-option="periodSeconds: 3"`)
+	command.Flags().StringVar(&s.args.ReadinessProbeAction, "readiness-probe-action", "", "the readiness probe action, support httpGet,exec,grpc,tcpSocket")
+	command.Flags().StringArrayVar(&readinessProbeActionOption, "readiness-probe-action-option", []string{}, `the readiness probe action option, usage: --readiness-probe-action-option="path: /healthz" or --readiness-probe-action-option="command=cat /tmp/healthy"`)
+	command.Flags().StringArrayVar(&s.args.ReadinessProbeOption, "readiness-probe-option", []string{}, `the readiness probe option, usage: --readiness-probe-option="initialDelaySeconds: 3" or --readiness-probe-option="periodSeconds: 3"`)
+	command.Flags().StringVar(&s.args.StartupProbeAction, "startup-probe-action", "", "the startup probe action, support httpGet,exec,grpc,tcpSocket")
+	command.Flags().StringArrayVar(&startupProbeActionOption, "startup-probe-action-option", []string{}, `the startup probe action option, usage: --startup-probe-action-option="path: /healthz" or --startup-probe-action-option="command=cat /tmp/healthy"`)
+	command.Flags().StringArrayVar(&s.args.StartupProbeOption, "startup-probe-option", []string{}, `the startup probe option, usage: --startup-probe-option="initialDelaySeconds: 3" or --startup-probe-option="periodSeconds: 3"`)
+
+	s.AddArgValue(LivenessProbeActionOption, &livenessProbeActionOption).
+		AddArgValue(ReadinessProbeActionOption, &readinessProbeActionOption).
+		AddArgValue(StartupProbeActionOption, &startupProbeActionOption)
 }
 
 func (s *CustomServingArgsBuilder) PreBuild() error {
@@ -91,6 +120,15 @@ func (s *CustomServingArgsBuilder) Build() error {
 	if err := s.check(); err != nil {
 		return err
 	}
+	if err := s.setLivenessProbeActionOption(); err != nil {
+		return err
+	}
+	if err := s.setReadinessProbeActionOption(); err != nil {
+		return err
+	}
+	if err := s.setStartupProbeActionOption(); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -109,4 +147,73 @@ func (s *CustomServingArgsBuilder) checkPortsIsOk() error {
 		return nil
 	}
 	return fmt.Errorf("all  ports are 0,invalid configuration.")
+}
+
+func (s *CustomServingArgsBuilder) setLivenessProbeActionOption() error {
+	value, ok := s.argValues[LivenessProbeActionOption]
+	if !ok {
+		return nil
+	}
+	livenessProbeActionOptions := value.(*[]string)
+	log.Debugf("livenessProbeActionOptions: %v", *livenessProbeActionOptions)
+	for _, option := range *livenessProbeActionOptions {
+		if strings.HasPrefix(option, "command=") {
+			s.args.LivenessProbeActionOption = append(s.args.LivenessProbeActionOption, "command:")
+			commands := strings.Split(option[8:], ` `)
+			for _, command := range commands {
+				cmd := fmt.Sprintf("- %s", command)
+				s.args.LivenessProbeActionOption = append(s.args.LivenessProbeActionOption, cmd)
+			}
+		} else {
+			s.args.LivenessProbeActionOption = append(s.args.LivenessProbeActionOption, option)
+		}
+	}
+
+	return nil
+}
+
+func (s *CustomServingArgsBuilder) setReadinessProbeActionOption() error {
+	value, ok := s.argValues[ReadinessProbeActionOption]
+	if !ok {
+		return nil
+	}
+	readinessProbeActionOptions := value.(*[]string)
+	log.Debugf("readinessProbeActionOptions: %v", *readinessProbeActionOptions)
+	for _, option := range *readinessProbeActionOptions {
+		if strings.HasPrefix(option, "command=") {
+			s.args.ReadinessProbeActionOption = append(s.args.ReadinessProbeActionOption, "command:")
+			commands := strings.Split(option[8:], ` `)
+			for _, command := range commands {
+				cmd := fmt.Sprintf("- %s", command)
+				s.args.ReadinessProbeActionOption = append(s.args.ReadinessProbeActionOption, cmd)
+			}
+		} else {
+			s.args.ReadinessProbeActionOption = append(s.args.ReadinessProbeActionOption, option)
+		}
+	}
+
+	return nil
+}
+
+func (s *CustomServingArgsBuilder) setStartupProbeActionOption() error {
+	value, ok := s.argValues[StartupProbeActionOption]
+	if !ok {
+		return nil
+	}
+	startupProbeActionOptions := value.(*[]string)
+	log.Debugf("startupProbeActionOptions: %v", *startupProbeActionOptions)
+	for _, lpo := range *startupProbeActionOptions {
+		if strings.HasPrefix(lpo, "command=") {
+			s.args.StartupProbeActionOption = append(s.args.StartupProbeActionOption, "command:")
+			commands := strings.Split(lpo[8:], ` `)
+			for _, command := range commands {
+				cmd := fmt.Sprintf("- %s", command)
+				s.args.StartupProbeActionOption = append(s.args.StartupProbeActionOption, cmd)
+			}
+		} else {
+			s.args.StartupProbeActionOption = append(s.args.StartupProbeActionOption, lpo)
+		}
+	}
+
+	return nil
 }
