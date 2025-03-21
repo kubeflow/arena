2c13f5c173257e40659d37594599a30e6bbfe1b6
Yi Chen <github@chenyicn.net>
2025-03-21 16:46:14 +0800

    Add flag --helm-binary for forward compatibility
    Signed-off-by: Yi Chen <github@chenyicn.net>

---
+++ pkg/util/helm/helm.go
@@ -0,0 +1,223 @@
+// Copyright 2018 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helm
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var helmCmd = []string{"arena-helm"}
+
+/**
+* install the release with cmd: helm install -f values.yaml chart_name
+ */
+func InstallRelease(name string, namespace string, values interface{}, chartName string) error {
+	binary, err := exec.LookPath(helmCmd[0])
+	if err != nil {
+		return err
+	}
+
+	// 1. generate the template file
+	valueFile, err := os.CreateTemp(os.TempDir(), "values")
+	if err != nil {
+		log.Errorf("Failed to create tmp file %v due to %v", valueFile.Name(), err)
+		return err
+	} else {
+		log.Debugf("Save the values file %s", valueFile.Name())
+	}
+	// defer os.Remove(valueFile.Name())
+
+	// 2. dump the object into the template file
+	err = toYaml(values, valueFile)
+	if err != nil {
+		return err
+	}
+
+	// 3. check if the chart file exists, if it's unix path, then check if it's exist
+	if strings.HasPrefix(chartName, "/") {
+		if _, err = os.Stat(chartName); os.IsNotExist(err) {
+			// TODO: the chart will be put inside the binary in future
+			return err
+		}
+	}
+
+	// 4. prepare the arguments
+	args := []string{"install", "-f", valueFile.Name(), "--namespace", namespace, name, chartName}
+	log.Debugf("Exec %s, %v", binary, args)
+
+	env := os.Environ()
+
+	// return syscall.Exec(cmd, args, env)
+	// 5. execute the command
+	cmd := exec.Command(binary, args...)
+	cmd.Env = env
+	out, err := cmd.CombinedOutput()
+	fmt.Println("")
+	fmt.Printf("%s\n", string(out))
+	if err != nil {
+		log.Fatalf("Failed to execute %s, %v with %v", binary, args, err)
+	}
+
+	// 6. clean up the value file if needed
+	if log.GetLevel() != log.DebugLevel {
+		err = os.Remove(valueFile.Name())
+		if err != nil {
+			log.Warnf("Failed to delete %s due to %v", valueFile.Name(), err)
+		}
+	}
+
+	return nil
+}
+
+/**
+* check if the release exist
+ */
+func CheckRelease(name string) (exist bool, err error) {
+	_, err = exec.LookPath(helmCmd[0])
+	if err != nil {
+		return exist, err
+	}
+
+	cmd := exec.Command(helmCmd[0], "get", name)
+	// support multiple cluster management
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("cmd.Start: %v", err)
+		return exist, err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				exitStatus := status.ExitStatus()
+				log.Debugf("Exit Status: %d", exitStatus)
+				if exitStatus == 1 {
+					err = nil
+				}
+			}
+		} else {
+			log.Fatalf("cmd.Wait: %v", err)
+			return exist, err
+		}
+	} else {
+		waitStatus := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		if waitStatus.ExitStatus() == 0 {
+			exist = true
+		} else {
+			if waitStatus.ExitStatus() != -1 {
+				return exist, fmt.Errorf("unexpected return code %d when exec helm get %s", waitStatus.ExitStatus(), name)
+			}
+		}
+	}
+
+	return exist, err
+}
+
+func DeleteRelease(name string) error {
+	binary, err := exec.LookPath(helmCmd[0])
+	if err != nil {
+		return err
+	}
+
+	args := []string{"del", "--purge", name}
+	cmd := exec.Command(binary, args...)
+
+	// return syscall.Exec(cmd, args, env)
+	out, err := cmd.Output()
+	log.Debugf("Delete release's result: %s\n", string(out))
+	return err
+}
+
+func ListReleases() (releases []string, err error) {
+	releases = []string{}
+	_, err = exec.LookPath(helmCmd[0])
+	if err != nil {
+		return releases, err
+	}
+
+	cmd := exec.Command(helmCmd[0], "list", "-q")
+	// support multiple cluster management
+	out, err := cmd.Output()
+	if err != nil {
+		return releases, err
+	}
+	return strings.Split(string(out), "\n"), nil
+}
+
+func ListReleaseMap() (releaseMap map[string]string, err error) {
+	releaseMap = map[string]string{}
+	_, err = exec.LookPath(helmCmd[0])
+	if err != nil {
+		return releaseMap, err
+	}
+
+	cmd := exec.Command(helmCmd[0], "list")
+	// support multiple cluster management
+	out, err := cmd.Output()
+	if err != nil {
+		return releaseMap, err
+	}
+	lines := strings.Split(string(out), "\n")
+
+	for _, line := range lines {
+		line = strings.Trim(line, " ")
+		if !strings.Contains(line, "NAME") {
+			cols := strings.Fields(line)
+			log.Debugf("%d cols: %v", len(cols), cols)
+			if len(cols) > 1 {
+				log.Debugf("releaseMap: %s=%s\n", cols[0], cols[len(cols)-1])
+				releaseMap[cols[0]] = cols[len(cols)-1]
+			}
+		}
+	}
+
+	return releaseMap, nil
+}
+
+func ListAllReleasesWithDetail() (releaseMap map[string][]string, err error) {
+	releaseMap = map[string][]string{}
+	_, err = exec.LookPath(helmCmd[0])
+	if err != nil {
+		return releaseMap, err
+	}
+
+	cmd := exec.Command(helmCmd[0], "list", "--all")
+	// support multiple cluster management
+	out, err := cmd.Output()
+	if err != nil {
+		return releaseMap, err
+	}
+	lines := strings.Split(string(out), "\n")
+
+	for _, line := range lines {
+		line = strings.Trim(line, " ")
+		if !strings.Contains(line, "NAME") {
+			cols := strings.Fields(line)
+			log.Debugf("%d cols: %v", len(cols), cols)
+			if len(cols) > 3 {
+				log.Debugf("releaseMap: %s=%s\n", cols[0], cols)
+				releaseMap[cols[0]] = cols
+			}
+		}
+	}
+
+	return releaseMap, nil
+}
---
+++ pkg/util/helm/helm_advanced.go
@@ -0,0 +1,84 @@
+// Copyright 2018 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helm
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+/**
+* generate helm template without tiller: helm template -f values.yaml chart_name
+* Exec /usr/local/bin/helm, [template -f /tmp/values313606961 --namespace default --name hj /charts/tf-horovod]
+* returns generated template file: templateFileName
+ */
+func GenerateHelmTemplateLegacy(name string, namespace string, valueFileName string, chartName string, options ...string) (templateFileName string, err error) {
+	tempName := fmt.Sprintf("%s.yaml", name)
+	templateFile, err := os.CreateTemp("", tempName)
+	if err != nil {
+		return templateFileName, err
+	}
+	templateFileName = templateFile.Name()
+
+	binary, err := exec.LookPath(helmCmd[0])
+	if err != nil {
+		return templateFileName, err
+	}
+
+	// 3. check if the chart file exists
+	// if strings.HasPrefix(chartName, "/") {
+	if _, err = os.Stat(chartName); err != nil {
+		return templateFileName, err
+	}
+	// }
+
+	// 4. prepare the arguments
+	args := []string{binary, "template", "-f", valueFileName,
+		"--namespace", namespace,
+		name,
+	}
+	if len(options) != 0 {
+		args = append(args, options...)
+	}
+
+	args = append(args, []string{chartName, ">", templateFileName}...)
+
+	log.Debugf("Exec bash -c %v", args)
+
+	// return syscall.Exec(cmd, args, env)
+	// 5. execute the command
+	log.Debugf("Generating template  %v", args)
+	cmd := exec.Command("bash", "-c", strings.Join(args, " "))
+	// cmd.Env = env
+	out, err := cmd.CombinedOutput()
+	fmt.Printf("%s", string(out))
+	if err != nil {
+		return templateFileName, fmt.Errorf("Failed to execute %s, %v with %v", binary, args, err)
+	}
+
+	// // 6. clean up the value file if needed
+	// if log.GetLevel() != log.DebugLevel {
+	// 	err = os.Remove(valueFileName)
+	// 	if err != nil {
+	// 		log.Warnf("Failed to delete %s due to %v", valueFileName, err)
+	// 	}
+	// }
+
+	return templateFileName, nil
+}
